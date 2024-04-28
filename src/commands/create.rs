b73ee51d140eb5d35e0c26f9000ec9976ce388a7
Costin Sin <sin.costinrobert@gmail.com>
2024-04-28 15:34:17 +0300

    Make git client accessible anywhere
    Add functions to get the mutex guard to avoid
    converting to IssueError every time someone wants
    to get a lock on the static mutexes.

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -1,12 +1,9 @@
 use eyre::{Ok, Result};
 
-use crate::{project::settings::PROJECT_SETTINGS, IssueError};
+use crate::project::settings::get_project_settings;
 
 pub fn create() -> Result<()> {
-    let trunk = PROJECT_SETTINGS
-        .lock()
-        .to_issue_error("Failed to get project settings mutex")?
-        .get_trunk()?;
+    let trunk = get_project_settings()?.get_trunk()?;
 
     println!("Trunk: {}", trunk);
 
--- src/git_client/mod.rs
+++ src/git_client/mod.rs
@@ -1,6 +1,24 @@
+use crate::IssueError;
+
+use self::git_cli::GitCli;
+use eyre::Result;
+use once_cell::sync::Lazy;
+use std::sync::{Mutex, MutexGuard};
+
 pub mod git_cli;
 
-pub trait GitClient {
+pub trait GitClient: Send + Sync {
     fn interactive_commit(&self);
     fn checkout(&self, branch: &str);
+}
+
+static GIT_CLIENT: Lazy<Mutex<Box<dyn GitClient>>> =
+    Lazy::new(|| Mutex::new(Box::new(GitCli::new().unwrap())));
+
+pub fn get_git_client() -> Result<MutexGuard<'static, Box<dyn GitClient>>> {
+    let guard = GIT_CLIENT
+        .lock()
+        .to_issue_error("Failed to get git client lock")?;
+
+    Ok(guard)
 }
