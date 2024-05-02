70e1f946f9e36ee5c8518660e303ced66ae76a41
Costin-Robert Sin <sin.costinrobert@gmail.com>
2024-05-02 13:41:46 +0300

    Add useful git client heplers
    - Interactive commit
    - Checkout
    - Create/Delete branch
    - Current commit title

--- src/git_client/git_cli.rs
+++ src/git_client/git_cli.rs
@@ -1,4 +1,4 @@
-use super::GitClient;
+use super::{CommitStatus, GitClient};
 use crate::git_provider::{get_provider_enum, SupportedProviders};
 use color_eyre::Section;
 use eyre::{Context, ContextCompat, Result};
@@ -23,8 +23,18 @@ impl GitCli {
 /// Implementation of the `GitClient` trait for the `GitCli` struct.
 impl GitClient for GitCli {
     /// Performs an interactive commit.
-    fn interactive_commit(&self) {
-        println!("Committing changes");
+    fn interactive_commit(&self) -> Result<CommitStatus> {
+        // Executes the `git commit -s` command to perform an interactive commit.
+        let status = Command::new("git")
+            .args(["commit", "-s"])
+            .status()
+            .context("Interactive commit failed")?;
+
+        if !status.success() {
+            return Ok(CommitStatus::Aborted);
+        }
+
+        Ok(CommitStatus::Success)
     }
 
     /// Checks out the specified branch.
@@ -32,8 +42,47 @@ impl GitClient for GitCli {
     /// # Arguments
     ///
     /// * `branch` - The name of the branch to check out.
-    fn checkout(&self, branch: &str) {
-        println!("Checking out branch: {}", branch);
+    fn checkout(&self, branch: &str) -> Result<()> {
+        // Executes the `git checkout <branch>` command to check out the specified branch.
+        let output = Command::new("git")
+            .args(["checkout", branch])
+            .output()
+            .context("Failed to checkout branch")?;
+
+        if !output.status.success() {
+            return Err(eyre::eyre!("Failed to checkout branch"));
+        }
+
+        Ok(())
+    }
+
+    /// Creates a new branch with the specified name.
+    ///
+    /// # Arguments
+    ///
+    /// * `branch` - The name of the new branch.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the name of the created branch on success, or an error on failure.
+    fn create_branch(&self, branch: &str) -> Result<String> {
+        // Executes the `git branch <branch>` command to create a new branch.
+        let output = Command::new("git").args(["branch", branch]).output();
+
+        match output {
+            Ok(output) => {
+                if output.status.success() {
+                    Ok(branch.to_string())
+                } else {
+                    // If the branch creation fails, appends a random string to the branch name and recursively calls `create_branch` again.
+                    let random_string = rand::random::<u8>().to_string();
+
+                    let new_branch = branch.to_string() + &random_string;
+                    Ok(self.create_branch(&new_branch)?)
+                }
+            }
+            Err(e) => Err(e).context("Failed to create a new branch"),
+        }
     }
 
     /// Retrieves information about the current Git repository.
@@ -49,6 +98,7 @@ impl GitClient for GitCli {
     /// * The `git` command fails to execute.
     /// * The current directory is not a Git repository.
     fn get_repository_info(&self) -> Result<(SupportedProviders, String, String)> {
+        // Executes the `git remote -v` command to get information about the remote repository.
         let out = Command::new("git")
             .args(["remote", "-v"])
             .output()
@@ -65,6 +115,7 @@ impl GitClient for GitCli {
                 .suggestion("Please check the connection to your remote repository")?,
         };
 
+        // Parses the remote repository URL to extract the provider, owner, and repository name.
         let regex = if is_https {
             Regex::new(r"https://(?P<provider>[^\.]+).*/(?P<owner>.+)/(?P<repo>.+).git").unwrap()
         } else {
@@ -83,7 +134,13 @@ impl GitClient for GitCli {
         Ok((provider, owner, repo))
     }
 
+    /// Retrieves the root directory of the current Git repository.
+    ///
+    /// # Returns
+    ///
+    /// The root directory of the current Git repository as an `Option<String>`.
     fn get_repository_root(&self) -> Option<String> {
+        // Executes the `git rev-parse --show-toplevel` command to get the root directory of the current Git repository.
         let root = Command::new("git")
             .args(["rev-parse", "--show-toplevel"])
             .output();
@@ -102,7 +159,13 @@ impl GitClient for GitCli {
         }
     }
 
+    /// Retrieves the name of the current branch.
+    ///
+    /// # Returns
+    ///
+    /// The name of the current branch as an `Option<String>`.
     fn get_current_branch(&self) -> Option<String> {
+        // Executes the `git symbolic-ref -q --short HEAD` command to get the name of the current branch.
         let branch = Command::new("git")
             .args(["symbolic-ref", "-q", "--short", "HEAD"])
             .output();
@@ -116,5 +179,44 @@ impl GitClient for GitCli {
             },
             Err(_) => None,
         }
+    }
+
+    /// Retrieves the title of the current commit.
+    ///
+    /// # Returns
+    ///
+    /// The title of the current commit as a `Result<String>`.
+    fn get_current_commit_title(&self) -> Result<String> {
+        // Executes the `git log -1 --pretty=%s` command to get the title of the current commit.
+        let output = Command::new("git")
+            .args(["log", "-1", "--pretty=%s"])
+            .output()
+            .context("Failed to get the current commit title")?;
+
+        let title = String::from_utf8(output.stdout)
+            .context("Failed to parse the current commit title")?
+            .trim()
+            .to_string();
+
+        Ok(title)
+    }
+
+    /// Deletes the specified branch.
+    ///
+    /// # Arguments
+    ///
+    /// * `branch` - The name of the branch to delete.
+    fn delete_branch(&self, branch: &str) -> Result<()> {
+        // Executes the `git branch -D <branch>` command to delete the specified branch.
+        let output = Command::new("git")
+            .args(["branch", "-D", branch])
+            .output()
+            .context("Failed to delete branch")?;
+
+        if !output.status.success() {
+            return Err(eyre::eyre!("Failed to delete branch"));
+        }
+
+        Ok(())
     }
 }
--- src/git_client/mod.rs
+++ src/git_client/mod.rs
@@ -8,12 +8,20 @@ use std::sync::{Mutex, MutexGuard};
 
 pub mod git_cli;
 
+pub enum CommitStatus {
+    Success,
+    Aborted,
+}
+
 pub trait GitClient: Send + Sync {
-    fn interactive_commit(&self);
-    fn checkout(&self, branch: &str);
+    fn interactive_commit(&self) -> Result<CommitStatus>;
+    fn checkout(&self, branch: &str) -> Result<()>;
+    fn create_branch(&self, branch: &str) -> Result<String>;
     fn get_repository_info(&self) -> Result<(SupportedProviders, String, String)>;
     fn get_repository_root(&self) -> Option<String>;
     fn get_current_branch(&self) -> Option<String>;
+    fn get_current_commit_title(&self) -> Result<String>;
+    fn delete_branch(&self, branch: &str) -> Result<()>;
 }
 
 static GIT_CLIENT: Lazy<Mutex<Box<dyn GitClient>>> =
